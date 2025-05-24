7fb83909935e242962951604f53875fa645bd16e
MuraMiki <imobatake29@gmail.com>
2025-05-24 20:55:40 +0900

    課題再提出

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class InputController {
 
+    //input-output-input(前に入力した値を表示）
+
     @GetMapping("/input")
     public String getInput(@RequestParam(name = "previous", required = false) String previous, Model model) {
+        if (previous != null && !previous.isEmpty()) { //追加　cuz リンクを押した後に、input.html戻った時に、前回入力された値は「abc」 というのがでていない、
+            model.addAttribute("previous", previous); //追加
+        }
 
         return "input";
 }
